f0bd704c9970e4252ef54a6576668c4ec048ad11
Frederick Aquino <xnox.45@hotmail.com>
2024-03-05 23:53:01 -0300

    removendo comentario

--- APIPlanetun/Program.cs
+++ APIPlanetun/Program.cs
@@ -1,10 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 var port = 7004;
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,7 +21,6 @@ builder
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
